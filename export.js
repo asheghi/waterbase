16d6edc854c899761889c5072b16f3fdb48ceef2
Seyyed Hassan Asheghi <asheghi.sh@gmail.com>
2021-01-21 14:42:04 +0330

    minor change

--- export.js
+++ export.js
@@ -1,30 +1,4 @@
+const {initSubscriptionServer} = require("./lib/utils");
 const {addModelToSubscription} = require("./lib/utils");
-const {execute, subscribe} = require('graphql')
-const {SubscriptionServer} = require('subscriptions-transport-ws')
 
-module.exports.addModelToSubscription = addModelToSubscription;
-module.exports.initSubscriptionServer = function (httpServer, keystone,) {
-    return new SubscriptionServer({
-            execute,
-            subscribe,
-            schema: keystone._schemas.public,
-            onConnect: (connectionPrams, webSocket) => {
-                //todo authorization based on AuthList
-                const {Authorization} = connectionPrams;
-                /*if (Authorization) {
-
-                }*/
-                let context = keystone.createContext({
-                    authentication: {
-                        item: {name: 'john', email: 'doe@gmail.com'},
-                        listKey: 'User',
-                    },
-                });
-                return context;
-            }
-        },
-        {
-            server: httpServer,
-            path: '/subscriptions'
-        })
-};
+module.exports = {initSubscriptionServer, addModelToSubscription};
